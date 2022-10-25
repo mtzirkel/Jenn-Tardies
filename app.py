d04db7db7f34d9cec9ca9c7611764987bca48054
hp <travisnmiller@gmail.com>
2022-10-25 13:35:51 -0600

    added app at root

--- app.py
+++ app.py
@@ -2,24 +2,21 @@ from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
-import pathlib
+from operator import add
 
 app = Dash(__name__)
 server = app.server
 
 
-def get_pandas_data(csv_filename: str) -> pd.DataFrame:
-    '''
-    Load data from /data directory as a pandas DataFrame
-    using relative paths. Relative paths are necessary for
-    data loading to work in Heroku.
-    '''
-    PATH = pathlib.Path(__file__).parent
-    DATA_PATH = PATH.joinpath("data").resolve()
-    return pd.read_csv(DATA_PATH.joinpath(csv_filename))
+def sort_days(year, trim_df):
+    if year_legend[year] in ['15-16', '16-17', '17-18']:
+        lst = ['M', 'T', 'W', 'R', 'F']
+    else:
+        lst = sorted(trim_df['day_of_week'].unique().tolist(), reverse=True)
+    return lst
 
 
-df = get_pandas_data("working_data.csv")
+df = pd.read_csv("data/working_data.csv")
 
 year_legend = {0: '15-16', 1: '16-17', 2: '17-18', 3: '18-19',
                4: '19-20', 5: '20-21', 6: '21-22'}
@@ -55,8 +52,7 @@ def update_graph(year, attendance_type, by_day):
         for period in periods:
             df_period = trim_df[trim_df['period'] == period]
             att_count = df_period[attendance_type].sum()
-            # if period == 'Advisory':
-            #     period = 0
+
             period_abs[period] = att_count
         df_output = pd.DataFrame({
             'Period': period_abs.keys(),
@@ -65,7 +61,8 @@ def update_graph(year, attendance_type, by_day):
 
         fig = px.bar(df_output, x='Period', y=f'Total {attendance_type}')
     else:
-        days = sorted(trim_df['day_of_week'].unique().tolist(), reverse=True)
+
+        days = sort_days(year, trim_df)
         data = []
         for day in days:
             trimed_df = trim_df[trim_df['day_of_week'] == day]
@@ -76,13 +73,27 @@ def update_graph(year, attendance_type, by_day):
                     period_lst.append(0)
                 else:
                     trimmer_df = trimed_df[trimed_df['period'] == str(period)]
-                    daily_total = trimmer_df['tardies'].sum()
+                    daily_total = trimmer_df[attendance_type].sum()
+
                     period_lst.append(daily_total)
 
             period_abs[day] = period_lst
-            data.append(
-                go.Bar(name=day, x=periods, y=period_abs[day])
-            )
+
+        if year_legend[year] != '18-19':
+            for d in period_abs.keys():
+                data.append(
+                    go.Bar(name=d, x=periods, y=period_abs[d])
+                )
+        else:
+            cleaned_period_abs = {
+                'M-R': [sum(x) for x in
+                        zip(period_abs['M-R'], period_abs['M'], period_abs['T'], period_abs['W'], period_abs['R'])],
+                'F': [sum(x) for x in zip(period_abs['F'], period_abs['2Fr'])]}
+
+            for day18_19 in cleaned_period_abs.keys():
+                data.append(
+                    go.Bar(name=day18_19, x=periods, y=cleaned_period_abs[day18_19])
+                )
 
         fig = go.Figure(data=data)
         fig.update_layout(barmode='group')
